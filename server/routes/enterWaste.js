97ba518da490a4707b97e7d01e21b945c7db115f
Jozzey <jozzey@hotmail.com>
2020-04-01 11:47:41 +0100

    Run linter

--- server/routes/enterWaste.js
+++ server/routes/enterWaste.js
@@ -1,18 +1,16 @@
 const joi = require('@hapi/joi')
 const data = require('../../data/data.json')
 
+const wasteName = []
 
-let wasteName = new Array()
-
-data.forEach(element => { 
+data.forEach(element => {
   wasteName.push(element.fields.wasteName)
 })
-
 
 module.exports = [{
   method: 'GET',
   path: '/enterWaste',
-  
+
   handler: (request, h) => {
     return h.view('enterWaste', {
       wasteName: wasteName
