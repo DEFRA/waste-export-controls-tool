a632e45b4f265128421f5717538674abe7bbd08f
Jozzey <jozzey@hotmail.com>
2020-04-16 11:20:11 +0100

    Try to post data from  autocomplete

--- server/routes/enterWaste.js
+++ server/routes/enterWaste.js
@@ -20,16 +20,19 @@ module.exports = [{
   method: 'POST',
   path: '/enterWaste',
   handler: (request, h) => {
+    const payload = request.payload
+    console.log(payload)
     return h.view('home', {
-      title: 'Hello',
-      message: 'World'
+      titleText: 'OMG has it worked:',
+      listItem1: 'If this is a waste type it has....',
+      listItem2: payload.selectedWasteName
     })
-  },
+  }/* ,
   options: {
     validate: {
       payload: joi.object().keys({
         email: joi.string().email().required()
       })
     }
-  }
+  } */
 }]
