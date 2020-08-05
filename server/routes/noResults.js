ff834c4e1e88a48b98cc5bc5d0d087951f704051
Jozzey <jozzey@hotmail.com>
2020-08-05 14:18:38 +0100

    tidied up

--- server/routes/noResults.js
+++ server/routes/noResults.js
@@ -1,26 +1,7 @@
-const joi = require('@hapi/joi')
-
 module.exports = [{
   method: 'GET',
   path: '/noResults',
   handler: (request, h) => {
-    request.yar.reset()
     return h.view('noResults')
-  }
-}, {
-  method: 'POST',
-  path: '/noResults',
-  handler: (request, h) => {
-    return h.view('noResults', {
-      title: 'Hello',
-      message: 'World'
-    })
-  },
-  options: {
-    validate: {
-      payload: joi.object().keys({
-        email: joi.string().email().required()
-      })
-    }
   }
 }]
