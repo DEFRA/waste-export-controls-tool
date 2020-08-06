76ef52db516759961c6c055e77ad5f7f2e6b7594
Jozzey <jozzey@hotmail.com>
2020-08-06 15:57:04 +0100

    sorted out analytics ID when on dev

--- server/plugins/analytics.js
+++ server/plugins/analytics.js
@@ -1,20 +1,17 @@
-// The analytics ID
-let analyticsId = 'UA-173909743-1'
+const analyticsId = []
 
 // Set a different analytics ID if in development
-if (process.env.NODE_ENV === 'development') {
-  analyticsId = 'xxx'
+if (process.env.NODE_ENV !== 'development') {
+  analyticsId.push({
+    id: 'UA-173909743-1',
+    hitTypes: ['pageview', 'event']
+  })
 }
 
 module.exports = {
   plugin: require('@defra/hapi-gapi'),
   options: {
-    propertySettings: [
-      {
-        id: analyticsId,
-        hitTypes: ['pageview', 'event']
-      }
-    ],
+    propertySettings: analyticsId,
     sessionIdProducer: async request => {
       // Would normally use the request object rather than "return 'test-session'" to retrieve the proper session identifier
       return request.yar.id
