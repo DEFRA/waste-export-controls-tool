e70128d683f13627432d2bcd3b891e2a0caade7e
Jozzey <jozzey@hotmail.com>
2020-08-20 15:42:36 +0100

    removed about page

--- server/plugins/router.js
+++ server/plugins/router.js
@@ -1,6 +1,5 @@
 const routes = [].concat(
   require('../routes/home'),
-  require('../routes/about'),
   require('../routes/public'),
   require('../routes/enterWaste'),
   require('../routes/multiWasteResults'),
